28848e1eb6fd0ff35fe913d8965f8b2111e29736
c-programming-ohm <74632417+c-programming-ohm@users.noreply.github.com>
2021-06-23 13:38:53 +0900

    Delete chapter02 directory

--- chapter02/2.1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	printf("Hello Programming in C World!\n");
-	return 0;
-}
--- chapter02/2.2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    /* メッセージを表示します */
-    printf("Hello Programming in C World!\n");
-
-    return 0;
-}
